510263abdf9c89d20fc7f90827d4bcd9d8b3354b
Marco Civello <marco.civello@gmail.com>
2021-04-01 18:38:38 +0200

    added all the film rows

--- src/Row.js
+++ src/Row.js
@@ -4,7 +4,7 @@ import axios from "./axios";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
-function Row({ title, fetchUrl }) {
+function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
@@ -16,6 +16,8 @@ function Row({ title, fetchUrl }) {
     fetchData();
   }, [fetchUrl]);
 
+  console.log(movies);
+
   return (
     <div className="row">
       <h2>{title}</h2>
@@ -23,9 +25,11 @@ function Row({ title, fetchUrl }) {
         {movies.map((movie) => (
           <img
             key={movie.id}
-            className="row__poster"
-            src={`${base_url}${movie.poster_path}`}
-            alt={movie.name}
+            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+            src={`${base_url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
+            alt={movie.title}
           />
         ))}
       </div>
